5b6df033b2549747c3bcb40ddb83658b96e49a78
thbang25 <83241507+thbang25@users.noreply.github.com>
2021-08-15 19:59:49 +0200

    Add files via upload
    SMBTHA002 main c file

--- main.c
+++ main.c
@@ -1,15 +1,11 @@
-# include <stdio.h> 
+# include <stdio.h>
+int main(){
+int a, b, sum;
 
-int main(){ 
-int a, b, sum; 
+printf("Enter a value for a: ");
+scanf("%d", &a);
 
-printf("Enter a value for a: "); 
-scanf("%d", &a); 
-
- printf("Enter a value for b: ");
- scanf("%d", &b);
-
-sum = a + b; 
-
-printf("The sum of a and b is %d \n",sum); 
-}
+printf("Enter a value for b: ");
+scanf("%d", &b);
+sum = a + b;
+printf("The sum of a and b is %d \n.",sum); }
